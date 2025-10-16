4062abc9def8d68bacac8e8af00d100108a383c8
mo7amed.talb <mo7amed.talb@gmail.com>
2025-10-16 22:37:58 +0100

    push 1-DATA/mutability/mutability.js

---
+++ 1-Data/4-mutability/mutability.js
@@ -0,0 +1,101 @@
+// const street = {
+//     number : 175,
+//     type : 'boulvard',
+//     name : 'matabiau',
+// }
+
+// const address = {
+//     country : 'France',
+//     town: 'toulouse',
+//     postalCode: 31000,
+//     street,
+// }
+
+// const userClement = {
+//     firstname: 'clement',
+//     lastname: 'Denis',
+//     email : 'mo7amed.talb@gamil.com',
+//     age: 29,
+//     address,
+// }
+
+// const users = [
+//     userClement,
+//     {
+//         firstname: 'moahmed',
+//         lastname: 'talb',
+//         email: 'mo7amed.talb@gamil.com',
+//         age: 21,
+//         address:
+//         {
+//             country: 'France',
+//             town: 'St-ouens',
+//             postalCode: 93000,
+//             street: 
+//             {
+//                 number: 78,
+//                 type: 'street',
+//                 name: 'Garibaldi',
+//             },
+//         },
+//     },
+// ]
+
+// const allowedCountries = ['France', 'Spain', 'Portugal', 'Russia', 'iclande']
+// const coords = [
+//     [32, 45],
+//     [-38, 57],
+//     [87, 99],
+//     [57, -2],
+//     [-74, -29],]
+
+// console.log(userClement['firstname'])
+// console.log(userClement['address']['town'])
+// console.log(userClement['address']['street'])
+// console.log(userClement['address']['street']['name'])
+
+// console.log("---------------------------------")
+
+// console.log(allowedCountries[0])
+// console.log(allowedCountries[2])
+// console.log(allowedCountries[3]);
+
+// console.log("---------------------------------")
+
+// console.log(coords[0][0]);
+// console.log(coords[0][1]);
+// console.log(coords[3][1]);
+// console.log(coords[3][0]);
+
+// console.log("---------------------------------")
+// // console.log(coords[143][0]) 
+
+
+// const mykey = 'name'
+// console.log(street[mykey])
+// console.log(userClement['last' + mykey])
+
+// const start = 1
+// let position = 0
+
+// console.log(allowedCountries[start + position++])
+// console.log(allowedCountries[start + position++])
+// console.log(allowedCountries[start + position++])
+
+// console.log("mohamed")
+
+// console.log(users[1].address.street.number)
+// console.log(users[0].address.street.number)
+
+const clone1 = {
+
+}
+
+const clone2 = {
+
+}
+
+const sameperson =
+{
+    
+}
